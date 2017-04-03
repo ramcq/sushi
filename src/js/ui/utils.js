7719326f8cc793d7942ccc7cd73c3a239d03ff72
Cosimo Cecchi <cosimoc@gnome.org>
2017-04-03 12:43:55 -0700

    utils: remove unused slowdown factor setting
    GTK has its own variable to control this if needed.

--- src/js/ui/utils.js
+++ src/js/ui/utils.js
@@ -30,12 +30,6 @@ const Lang = imports.lang;
 
 const Constants = imports.util.constants;
 
-let slowDownFactor = 0;
-
-function setSlowDownFactor(factor) {
-    slowDownFactor = factor;
-}
-
 function getScaledSize(baseSize, allocSize, upscale) {
     let allocW = allocSize[0];
     let allocH = allocSize[1];
